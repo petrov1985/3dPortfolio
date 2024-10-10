c6ea68f3073e4bf1c4c02213f2b58fc9c99ca9d6
Petar Petrov <peterpetrov1985@gmail.com>
2024-10-10 12:52:26 +0200

    initial commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,64 @@
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { PerspectiveCamera } from '@react-three/drei'
+import HackerRoom from '../components/HackerRoom'
+import CanvasLoader from '../components/CanvasLoader'
+import { useMediaQuery } from 'react-responsive'
+import { calculateSizes } from '../constants/index'
+import Target from '../components/Target'
+import ReactLogo from '../components/ReactLogo'
+import Cube from '../components/Cube'
+import Rings from '../components/Rings'
+import HeroCamera from '../components/HeroCamera'
+import Button from '../components/Button'
+
+const Hero = () => {
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1180 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+    
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+                Hi, I'm Peter <span className='waving-hand'>👋</span></p>
+            <p className='hero_tag text-gray_gradient'>Building Products & Brands</p>
+        </div>
+
+        <div className="w-full h-full absolute inset-0">
+            {/* <Leva /> */}
+            <Canvas className='w-full h-full'> 
+                <Suspense fallback={<CanvasLoader />}>
+              <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+
+              <HeroCamera isMobile={isMobile}>
+              <HackerRoom 
+              position={sizes.deskPosition} 
+              scale={sizes.deskScale}
+              rotation={[0, -Math.PI, 0]}
+              />
+              </HeroCamera>
+              <group>
+                <Target position={sizes.targetPosition} />
+                <ReactLogo position={sizes.reactLogoPosition}/>
+                <Cube position={sizes.cubePosition}/>
+                <Rings position={sizes.ringPosition}/>
+              </group>
+
+              <ambientLight intensity={1}/>
+              <directionalLight  position={[10, 10, 10]} intensity={0.5} />
+                </Suspense>
+            </Canvas>
+        </div>
+        <div className='absolute bottom-7 left-0 right-0 w-full z-10 c-space'>
+          <a href='#contact' className='w-fit'>
+            <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
+          </a>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
